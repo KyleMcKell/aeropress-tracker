4f87afbe06d0b0a06e370b64e6402ea028cd9d1b
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-12-16 14:56:28 -0700

    added in prisma and nextauth to project, also added in Google Provider for nextauth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+import { SessionProvider } from 'next-auth/react';
+
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+	return (
+		<SessionProvider session={session}>
+			<Component {...pageProps} />
+		</SessionProvider>
+	);
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
 import type { NextPage } from 'next';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 const Home: NextPage = () => {
+	const { data: session } = useSession();
+	if (session) {
+		return (
+			<>
+				Signed in as {session.user?.email} <br />
+				<button onClick={() => signOut()}>Sign out</button>
+			</>
+		);
+	}
 	return (
-		<div>
-			<h1>hi there</h1>
-		</div>
+		<>
+			Not signed in <br />
+			<button onClick={() => signIn()}>Sign in</button>
+		</>
 	);
 };
 
