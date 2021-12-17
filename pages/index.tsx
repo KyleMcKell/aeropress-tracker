fe0006e119406fe979d0eb042abe64e03cae3ffc
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-12-16 23:02:39 -0700

    added getServerSideProps to make sure the flicker doesn't happen when loading protected routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,36 @@
-import type { NextPage } from 'next';
-import { useSession, signIn, signOut } from 'next-auth/react';
+import type { GetServerSideProps, NextPage } from 'next';
+import { getSession, useSession, signIn, signOut } from 'next-auth/react';
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+	return {
+		props: {
+			session: await getSession(context),
+		},
+	};
+};
 
 const Home: NextPage = () => {
-	const { data: session } = useSession();
-	if (session) {
+	const { data: session, status } = useSession();
+	if (status === 'authenticated') {
+		return (
+			<div style={{ backgroundColor: 'slateblue' }}>
+				Signed in as {session?.user?.email} <br />
+				<button onClick={() => signOut()}>Sign out</button>
+			</div>
+		);
+	}
+	if (status === 'unauthenticated') {
 		return (
 			<>
-				Signed in as {session.user?.email} <br />
-				<button onClick={() => signOut()}>Sign out</button>
+				Not signed in <br />
+				<button onClick={() => signIn()}>Sign in</button>
 			</>
 		);
 	}
-	return (
-		<>
-			Not signed in <br />
-			<button onClick={() => signIn()}>Sign in</button>
-		</>
-	);
+	if (status === 'loading') {
+		return <div>Loading...</div>;
+	}
+	return <div>Error</div>;
 };
 
 export default Home;
