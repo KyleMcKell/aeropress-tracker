8ba08dbbbed4989630e763f6138aee098ce3bf0a
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-12-19 15:48:46 -0700

    added post ability for brews

--- pages/api/brew/index.ts
+++ pages/api/brew/index.ts
@@ -3,7 +3,8 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '~/lib/prisma';
 
 type Data = {
-	brews: AeropressBrew[];
+	brews?: AeropressBrew[];
+	brew?: AeropressBrew;
 };
 
 const userActions = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
@@ -15,6 +16,44 @@ const userActions = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 			}
 			res.status(200).json({ brews });
 		}
+
+		case 'POST': {
+			const {
+				name,
+				brewTime,
+				waterTemp,
+				coffeeWeight,
+				waterWeight,
+				grindSize,
+				roastType,
+				inverted,
+				favorite,
+				instructions,
+				description,
+				userId,
+			}: AeropressBrew = req.body;
+			const brew = await prisma.aeropressBrew.create({
+				data: {
+					name,
+					brewTime,
+					waterTemp,
+					coffeeWeight,
+					waterWeight,
+					grindSize,
+					roastType,
+					inverted,
+					favorite,
+					instructions,
+					description,
+					userId,
+				},
+			});
+			if (!brew) {
+				return res.status(400);
+			}
+			res.status(200).json({ brew });
+		}
+
 		default: {
 			res.status(405).end();
 		}
