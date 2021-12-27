53ca71d8bd16f89ec2ded73dbc34006e2ccc1002
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-12-27 11:05:47 -0700

    removed test data lol

--- components/BrewCard/BrewCard.tsx
+++ components/BrewCard/BrewCard.tsx
@@ -8,24 +8,24 @@ interface Props {
 	showTimer?: boolean;
 }
 
-const dataToUse = {
-	id: 1,
-	name: 'Test Brew',
-	brewTime: 120,
-	waterTemp: 100,
-	coffeeWeight: 15,
-	waterWeight: 200,
-	grindSize: 'Coarse',
-	roastType: 'Medium Light',
-	inverted: false,
-	description: 'This brew is very very tasty',
-	favorite: false,
-	instructions: 'asdfasdf asdfasdfa',
-	userId: 1,
-};
+// const dataToUse = {
+// 	id: 1,
+// 	name: 'Test Brew',
+// 	brewTime: 120,
+// 	waterTemp: 100,
+// 	coffeeWeight: 15,
+// 	waterWeight: 200,
+// 	grindSize: 'Coarse',
+// 	roastType: 'Medium Light',
+// 	inverted: false,
+// 	description: 'This brew is very very tasty',
+// 	favorite: false,
+// 	instructions: 'asdfasdf asdfasdfa',
+// 	userId: 1,
+// };
 
 const BrewCard = ({ brew, user, showTimer = true }: Props) => {
-	brew = dataToUse;
+	// brew = dataToUse;
 
 	const {
 		name: brewName,
