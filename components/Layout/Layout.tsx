ebdfa2ac3f5fdf36463c09347b8ca3279c46a57a
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-12-27 23:58:40 -0700

    got rid of sessionmounter

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head';
 import React from 'react';
 
 import Header from '~/components/Header';
-import SessionMounter from '~/components/SessionMounter';
 
 interface Props {
 	title: string;
