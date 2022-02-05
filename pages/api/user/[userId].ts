66835d990e01c1e2b1902a983bedf4d6adb443af
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2022-02-05 10:52:03 -0700

    changed api to expose the first name, and id of the users, but nothing else

--- pages/api/user/[userId].ts
+++ pages/api/user/[userId].ts
@@ -12,26 +12,29 @@ const userActions = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 	const { userId } = req.query;
 	const session = await getSession({ req });
 
-	const userIdInt = parseInt(String(userId));
+	const userIdInt = Number(userId);
 
-	if (session?.userId === userIdInt) {
-		switch (req.method) {
-			case 'GET': {
-				const user = await prisma.user.findUnique({
-					where: { id: userIdInt },
-				});
-				if (!user) {
-					return res.status(400);
-				}
+	switch (req.method) {
+		case 'GET': {
+			const user = await prisma.user.findUnique({
+				where: { id: userIdInt },
+			});
+			if (!user) {
+				return res.status(400);
+			} else if (session?.userId === userIdInt) {
+				return res.status(200).json({ user });
+			} else {
+				user.email = 'coffeelover@aeropresstracker.com';
+				user.image = "you sneakster, you aren't supposed to be here";
+				user.emailVerified = null;
+				user.name =
+					user.name === null ? 'Stealth Barista' : user.name.split(' ')[0];
 				return res.status(200).json({ user });
 			}
-			default: {
-				return res.status(405).end();
-			}
 		}
-	} else {
-		return res.status(401).end();
+		default: {
+			return res.status(405).end();
+		}
 	}
 };
-
 export default userActions;
